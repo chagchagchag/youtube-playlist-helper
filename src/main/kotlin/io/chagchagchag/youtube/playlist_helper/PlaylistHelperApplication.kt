11179acd63c7af3e93b3e5a7f5c9713863250772
chagchagchag <chagchagchag.dev@gmail.com>
2024-06-11 22:55:54 +0900

    first commit

---
+++ src/main/kotlin/io/chagchagchag/youtube/playlist_helper/PlaylistHelperApplication.kt
@@ -0,0 +1,11 @@
+package io.chagchagchag.youtube.playlist_helper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlaylistHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlaylistHelperApplication>(*args)
+}
---
+++ src/test/kotlin/io/chagchagchag/youtube/playlist_helper/PlaylistHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.chagchagchag.youtube.playlist_helper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaylistHelperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
