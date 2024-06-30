c0b738b0c7fbedb5d1fdda619a81d97cdacad975
chagchagchag <chagchagchag.dev@gmail.com>
2024-07-01 08:16:40 +0900

    style : line

--- yph-api-client/src/main/kotlin/io/chagchagchag/youtube/playlist_helper/api_client/domain/playlist/dto/PlaylistsResponse.kt
+++ yph-api-client/src/main/kotlin/io/chagchagchag/youtube/playlist_helper/api_client/domain/playlist/dto/PlaylistsResponse.kt
@@ -3,7 +3,6 @@ package io.chagchagchag.youtube.playlist_helper.api_client.domain.playlist.dto
 import io.chagchagchag.youtube.playlist_helper.api_client.global.youtube.model.PageInfo
 import io.chagchagchag.youtube.playlist_helper.api_client.global.youtube.model.PlaylistItem
 
-
 data class PlaylistsResponse(
     val kind: String,
     val etag: String,
