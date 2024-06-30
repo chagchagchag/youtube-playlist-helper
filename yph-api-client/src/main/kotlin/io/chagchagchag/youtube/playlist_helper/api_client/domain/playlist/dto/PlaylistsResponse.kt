9d36275f831dbd9155be8dff8502e92a6cc8d754
chagchagchag <chagchagchag.dev@gmail.com>
2024-07-01 08:15:59 +0900

    chore : 주석 제거

--- yph-api-client/src/main/kotlin/io/chagchagchag/youtube/playlist_helper/api_client/domain/playlist/dto/PlaylistsResponse.kt
+++ yph-api-client/src/main/kotlin/io/chagchagchag/youtube/playlist_helper/api_client/domain/playlist/dto/PlaylistsResponse.kt
@@ -7,6 +7,6 @@ import io.chagchagchag.youtube.playlist_helper.api_client.global.youtube.model.P
 data class PlaylistsResponse(
     val kind: String,
     val etag: String,
-    val pageInfo: PageInfo, // TODO : 헐 이거 제공해주네... 나중에 커스텀 자료로 변경하기
-    val items: List<PlaylistItem>, // TODO : 헐 이거 제공해주네... 나중에 커스텀 자료로 변경하기
+    val pageInfo: PageInfo,
+    val items: List<PlaylistItem>,
 )
